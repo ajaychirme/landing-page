62d7010226135ef28aed86df130be2bc2aab45da
ajaychirme <ajaychirme84@gmail.com>
2025-11-12 14:35:51 +0530

    add verification banner and black friday toggle 4

--- src/TotumNavbar.jsx
+++ src/TotumNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import brandingLogo from "./branding-image.svg";
 
 const TotumNavbar = ({ isLoggedIn, setIsLoggedIn }) => {
