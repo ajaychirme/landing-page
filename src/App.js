9c4cbb69f3d9e28b507a517da6a827a9c245a1a3
ajaychirme <ajaychirme84@gmail.com>
2025-11-12 09:36:16 +0530

    margi reduced 3

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import LandingPage from './LandingPage'
 import Portfolio from './Portfolio.jsx'
 import Portfolio1 from './Portfolio1.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Portfolio2 from './Portfolio2.jsx'
-import Portfolio3 from './Portfolio3.jsx'
+import Portfolio2 from './Portfolio2.jsx';
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path="/saurabhgalgale" element={<Portfolio />} />
         <Route path="/saurabhgalgale1" element={<Portfolio1 />} />
         <Route path="/saurabhgalgale2" element={<Portfolio2 />} />
-        <Route path="/saurabhgalgale3" element={<Portfolio3 />} />
       </Routes>
     </BrowserRouter>
   );
